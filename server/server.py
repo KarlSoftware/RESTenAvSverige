9aad5033a768b9a9288ba3cfea50aa8526cf4953
Calle Svensson <calle.svensson@zeta-two.com>
2017-03-11 18:34:09 +0100

    API yrkesgrupper

--- server/server.py
+++ server/server.py
@@ -1,5 +1,6 @@
 import psycopg2
-from flask import Flask, request
+import json
+from flask import Flask, request, jsonify
 from config import conn
 app = Flask(__name__)
 
@@ -9,25 +10,25 @@ def index():
 
 @app.route("/geografi")
 def geografi():
-	print(conn)
-	with conn.cursor() as cursor:
-		query = "SELECT kommunnamn, kommunkod, lansnamn, lanskod, booli_id FROM RESTenAvSverige.kommuner"
-    	
-    	result = {}
-    	for kommun in cursor:
-    		if kommun['kommunkod'] not in result:
-    			result[kommun['kommunkod']] = {
-    				'lansnamn': kommun['lansnamn'],
-    				'lanskod': kommun['lanskod'],
-    				'kommuner': {}
-    			}
+    with conn.cursor() as cursor:
+        query = "SELECT kommunnamn, kommunkod, lansnamn, lanskod, booli_id FROM h4s.kommuner"
+        cursor.execute(query)
 
-    		result[kommun['kommunkod']]['kommuner'].append({
-    			'kommunnamn': kommun['kommunnamn'],
-    			'kommunkod': kommun['kommunkod']
-			})
+        result = {}
+        for kommun in cursor:
+            if kommun['kommunkod'] not in result:
+                result[kommun['kommunkod']] = {
+                    'lansnamn': kommun['lansnamn'],
+                    'lanskod': kommun['lanskod'],
+                    'kommuner': {}
+                }
 
-    	return json.dumps(result)
+            result[kommun['kommunkod']]['kommuner'].append({
+                'kommunnamn': kommun['kommunnamn'],
+                'kommunkod': kommun['kommunkod']
+            })
+
+        return json.dumps(result)
 
 @app.route("/search")
 def search():
@@ -35,7 +36,11 @@ def search():
 
 @app.route("/yrkesgrupper")
 def yrkesgrupper():
-    return "Hello World!!"
+    with conn.cursor() as cursor:
+        query = "SELECT yrkesgrupp_id, yrkesgrupp FROM h4s.yrkesgrupper"
+        cursor.execute(query)
+        result = map(lambda x: {'yrkesgrupp_id': x[0], 'yrkesgrupp': x[1]}, cursor.fetchall())
+        return jsonify(result)
 
 
 if __name__ == "__main__":
