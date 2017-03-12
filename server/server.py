0333e3c0b1eefae8457979cd977402c1d8179f69
Calle Svensson <calle.svensson@zeta-two.com>
2017-03-12 01:23:54 +0100

    Fixed name normalization

--- server/server.py
+++ server/server.py
@@ -5,12 +5,12 @@ from config import conn
 app = Flask(__name__)
 
 def normalize_county_name(lansnamn):
-    lansnamn = lansnamn.replace(' län','').replace(' ','').lower().replace('å','a').replace('ä','a').replace('ö','o')
+    lansnamn = lansnamn.replace(' län','').replace(' ','').lower().replace('å','a').replace('ä','a').replace('ö','o').replace('-','')
     lansnamn = lansnamn[:-1] if lansnamn[-1] == 's' else lansnamn
     return lansnamn
 
 def normalize_munip_name(kommunnamn):
-    kommunnamn = kommunnamn.replace(' ','').lower().replace('å','a').replace('ä','a').replace('ö','o')
+    kommunnamn = kommunnamn.replace(' ','').lower().replace('å','a').replace('ä','a').replace('ö','o').replace('-','')
     kommunnamn = kommunnamn[:-1] if kommunnamn[-1] == 's' else kommunnamn
     return kommunnamn
 
@@ -52,6 +52,7 @@ def search():
 
     with conn.cursor() as cursor:
         query = "SELECT antal_nara, antal_exakt, kommunkod, lanskod FROM h4s.platsbank WHERE yrkesgrupp_id IN (%s)" % (','.join(map(lambda x: str(int(x)), yrkesgrupper_id)))
+        print('Query: %s' % query)
         cursor.execute(query)
 
         # Choose exact or nearby
