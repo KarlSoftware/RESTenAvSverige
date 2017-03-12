1eb1dd7184e41c24dc86fba9c7034a0a1b3ba80d
Veronica <veronica.ginman@gmail.com>
2017-03-12 04:04:32 +0100

    Update name-change

--- webgui/src/components/map/index.js
+++ webgui/src/components/map/index.js
@@ -25,7 +25,7 @@ export default class Map extends React.Component {
       <div className={styles.main}>
         <svg className={styles.svg} xmlns="http://www.w3.org/2000/svg" viewBox="-8449.69 -8449.69 692875 1.57668e+006">
           <g transform="translate(0,1.55978e+006) scale(1, -1)">
-            { _.map(this.props.parts, (value, key) => <County key={key} status={self.props.result ? self.props.result[key].goodness : 1} value={value} onMouseOver={() => this.onHover(key)} />) }
+            { _.map(this.props.parts, (value, key) => <County key={key} status={self.props.result ? self.props.result[key].goodness : 1} value={value} onMouseOver={() => this.props.onHover(key)} />) }
           </g>
         </svg>
       </div>
